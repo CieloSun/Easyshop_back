cb8313748c5dfff413f0f25bfc3b69db12105bea
CieloSun <632898354@qq.com>
2016-12-23 22:55:53 +0800

    Add missing packages Begin to wrte the controller part(may change after the doc finished)

---
+++ src/com/jimstar/easyshop/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.jimstar.easyshop.controller;
+
+import com.jimstar.easyshop.service.UserCustomerService;
+import com.jimstar.easyshop.service.UserMerchantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
+
+/**
+ * Created by 63289 on 2016/12/23.
+ */
+@Controller
+@RequestMapping("/User")
+public class UserController {
+    @Autowired
+    private UserCustomerService userCustomerService;
+    @Autowired
+    private UserMerchantService userMerchantService;
+    @RequestMapping("/firstPage")
+    public String firstPage(){
+        return "firstPage";
+    }
+    @RequestMapping(value="/login",method = RequestMethod.POST)
+    @ResponseBody
+    public String login(@RequestBody Map<String,Object> map) throws Exception{
+
+        //TODO
+        return null;
+    }
+}
