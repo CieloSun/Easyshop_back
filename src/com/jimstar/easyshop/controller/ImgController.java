4360126b5637026b946d9dca7d447807735150b5
CieloSun <632898354@qq.com>
2016-12-23 21:06:05 +0800

    sth wrong with springMVC

--- src/com/jimstar/easyshop/controller/ImgController.java
+++ src/com/jimstar/easyshop/controller/ImgController.java
@@ -3,11 +3,13 @@ package com.jimstar.easyshop.controller;
 import com.jimstar.easyshop.service.ImgService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by 63289 on 2016/12/23.
  */
 @Controller
+@RequestMapping("/Main")
 public class ImgController {
     @Autowired
     private ImgService imgService;
