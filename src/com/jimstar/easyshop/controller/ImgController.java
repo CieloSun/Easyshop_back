4937bcc40224486edd10cdbd286c2b82c2f4ec67
CieloSun <632898354@qq.com>
2016-12-25 11:20:43 +0800

    fix imgDao

--- src/com/jimstar/easyshop/controller/ImgController.java
+++ src/com/jimstar/easyshop/controller/ImgController.java
@@ -1,7 +1,10 @@
 package com.jimstar.easyshop.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.jimstar.easyshop.entity.Img;
 import com.jimstar.easyshop.service.ImgService;
 import com.jimstar.easyshop.util.DigestUtil;
+import com.jimstar.easyshop.util.JSONUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -11,7 +14,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.portlet.ModelAndView;
 
-import java.io.InputStream;
+import java.io.IOException;
+import java.lang.String;
+import java.util.Map;
+
+import static com.sun.corba.se.spi.activation.IIOP_CLEAR_TEXT.value;
 
 @Controller
 @RequestMapping("/Img")
@@ -25,9 +32,11 @@ public class ImgController {
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
     @ResponseBody
-    public ModelAndView add(@RequestBody InputStream value, ModelMap modelMap) {
-        ModelAndView modelAndView=new ModelAndView();
-        String id = null;
+    public String add(@RequestBody String mapString, ModelMap modelMap) throws Exception {
+        Map map=JSONUtil.parseMap(mapString);
+        String value=(String) map.get("value");
+        Img img=imgService.addAnImageByValue(value);
+        String id=img.getId();
         if (id != null) {
             modelMap.addAttribute("status",0);
             modelMap.addAttribute("info","Success to save the image");
@@ -37,19 +46,18 @@ public class ImgController {
             modelMap.addAttribute("status",1);
             modelMap.addAttribute("info","Fail to save the image");
         }
-        modelAndView.addAllObjects(modelMap);
-        return modelAndView;
+        return JSONUtil.toJSON(modelMap);
     }
 
     @RequestMapping(value = "/get")
     @ResponseBody
-    public ModelAndView get(String id, ModelMap modelMap){
-        ModelAndView modelAndView=new ModelAndView();
+    public String get(@RequestBody String mapString, ModelMap modelMap) throws Exception {
+        Map map=JSONUtil.parseMap(mapString);
+        String id=(String) map.get("id");
         System.err.println("ImgID=" + id);
         String value = imgService.getImgById(id).getValue();
         modelMap.addAttribute("value", value);
-        modelAndView.addAllObjects(modelMap);
-        return modelAndView;
+        return JSONUtil.toJSON(modelMap);
     }
 
 }
---
+++ src/com/jimstar/easyshop/controller/OrderController.java
@@ -0,0 +1,12 @@
+package com.jimstar.easyshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by 63289 on 2016/12/25.
+ */
+@Controller
+@RequestMapping("/Order")
+public class OrderController {
+}
